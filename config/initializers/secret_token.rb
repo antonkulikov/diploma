7df54de71e72c3c604c2fc01ba172e3988e4eb93
Anton Kulikov <avkulikov89@gmail.com>
2013-06-24 12:07:55 +0400

    Initial commit. Blank App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diploma::Application.config.secret_token = '152bffe4c4237aaf4bc2280d3d9b6d4808dba98662008536d6a96fc7541e26982a0b587631b7ebae0dcde0cfc66e1a169dac1fe50c8bed552008c4eda17640ba'
